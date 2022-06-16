0b51e48ed4bede6351ff49dfdf7ec1c563dc74c4
ajit <mobile.developer@ongraph.com>
2022-06-16 18:52:58 +0530

    upd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,23 @@
+// import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 // import 'package:news_app/page/user_list_page.dart';
 
 // void main() {
 //   testWidgets("testing widgets", (WidgetTester tester) async {
-//     final tab1 = find.byKey(const ValueKey("tab1"));
-//     final tab2 = find.byKey(const ValueKey("tab2"));
-
 //     await tester.pumpWidget(const UserListPage());
-//     final tab1 = find.byType(Tab);
 
-//     expect(tab1, findsOneWidget);
-//     expect(find.text('mojombo'), findsOneWidget);
-//     expect(find.widgetWithText(Type(), text), findsOneWidget);
+//     var tabBar = find.byType(TabBar);
+//     expect(tabBar, findsOneWidget);
+//     // final tab1 = find.byKey(const ValueKey("tab1"));
+//     // final tab2 = find.byKey(const ValueKey("tab2"));
 
-//     await tester.pumpWidget(const UserListPage());
+//     // await tester.pumpWidget(const UserListPage());
+//     // final tab1 = find.byType(Tab);
+
+//     // expect(tab1, findsOneWidget);
+//     // expect(find.text('mojombo'), findsOneWidget);
+//     // expect(find.widgetWithText(Type(), text), findsOneWidget);
+
+//     // await tester.pumpWidget(const UserListPage());
 //   });
 // }
